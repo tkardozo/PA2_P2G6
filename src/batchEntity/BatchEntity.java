76b1b3c5fd970e31506adcc6297cf147ee5e4f84
danielnottaken <danielmadail@msn.com>
2019-04-22 17:03:38 +0100

    Batch part

--- src/batchEntity/BatchEntity.java
+++ src/batchEntity/BatchEntity.java
@@ -2,12 +2,15 @@ package batchEntity;
 
 import kafka.SimpleConsumer;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
 import static kafka.common.SERVERS;
 import static kafka.common.TOPIC_TO_Batch;
 
 public class BatchEntity {
-    public static void main(String[] args) {
-        BatchWorker worker = new BatchWorker();
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        BatchWorker worker = new BatchWorker("src/data/batchLogs.txt");
         SimpleConsumer<String> consumer = new SimpleConsumer<>(SERVERS, worker);
         consumer.subscribe(TOPIC_TO_Batch);
         consumer.run();
--- src/batchEntity/BatchWorker.java
+++ src/batchEntity/BatchWorker.java
@@ -2,10 +2,27 @@ package batchEntity;
 
 import kafka.Callback;
 
+import java.io.*;
+
 public class BatchWorker implements Callback<String> {
+
+    private Writer writer;
+    public BatchWorker (String file) throws FileNotFoundException, UnsupportedEncodingException {
+        this.writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
+    }
 
     @Override
     public void onSuccess(String key, String value) {
+
         System.out.println("Received: " + value);
+
+        //gui
+        //textArea.append(value + "\n");
+
+        try {
+            writer.write(value + "\n");
+        } catch (IOException e) {
+        }
     }
+
 }
