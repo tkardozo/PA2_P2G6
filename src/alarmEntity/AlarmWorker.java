fa87b70bc1b09bef022736cfc241514caecb9321
tkardozo <t.cardoso@ua.pt>
2019-04-22 17:33:43 +0100

    alarm worker

--- src/alarmEntity/AlarmWorker.java
+++ src/alarmEntity/AlarmWorker.java
@@ -2,10 +2,31 @@ package alarmEntity;
 
 import kafka.Callback;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AlarmWorker implements Callback<String> {
 
     @Override
     public void onSuccess(String key, String value) {
-        System.out.println("Received: " + value);
+        List<String> msg = new ArrayList<>( Arrays.asList( value.split("[|]") ) );
+
+        Integer speed = Integer.parseInt(msg.get(4));
+        Integer maxSpeed = Integer.parseInt(msg.get(6));
+
+        if(speed > maxSpeed){
+            msg.add("ON");
+        }else{
+            msg.add("OFF");
+        }
+
+        StringBuilder result = new StringBuilder();
+        msg.forEach(part->{
+            if(result.length() > 0)
+                result.append("|");
+            result.append(part);
+        });
+        System.out.println(result.toString());
     }
 }
